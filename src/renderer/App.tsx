c652b2d3726b36c0150d5167ab5e0eaaa0ce8df7
ayonshafiul <ayonshafiul@gmail.com>
2022-09-19 15:34:14 -0700

    Add xterm, monaco editor, node-pty

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,17 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import './App.css';
-import CodeMirror from '@uiw/react-codemirror';
-import { python } from '@codemirror/lang-python';
-// Import XTerm
+import Editor from '@monaco-editor/react';
 import { XTerm } from 'xterm-for-react';
 
 const Hello = () => {
   const code = 'const a = 0;';
   return (
     <div style={{ fontSize: '16px', fontWeight: 'bold' }}>
-      <CodeMirror
+      <Editor
         height="50vh"
-        theme="dark"
-        extensions={[python()]}
-        indentWithTab={true}
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
       />
       <XTerm />
     </div>
